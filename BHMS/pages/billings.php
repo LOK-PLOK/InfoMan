a4dab409fb06d537bf7724e145ff54a324cdf7bb
Luis <110468848+LowisWano@users.noreply.github.com>
2024-07-04 23:26:16 +0800

    Everything is broken

--- BHMS/pages/billings.php
+++ BHMS/pages/billings.php
@@ -220,14 +220,12 @@
 <!-- Additional JavaScript -->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js" integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="/js/date.js">
-    
-</script>
+<script src="/js/date.js"></script>
 <script src="/js/prepopulate.js"></script>
 <script src="/js/checkbox.js"></script>
 <script src="/js/date.js"></script>
-<script>
 
+<script>
     const payOccType = document.getElementById('payment-occupanyType');
     const addPaymentBtn = document.getElementById('add-payment-button');
     const noOfAppliances = document.getElementById('noOfAppliances');
@@ -271,7 +269,6 @@
     
     document.addEventListener('DOMContentLoaded', function () {
     const deleteModal = new bootstrap.Modal(document.getElementById('deleteBillingsModal'));
-
     const deleteButtons = document.querySelectorAll('.delete-button');
     deleteButtons.forEach(button => {
         button.addEventListener('click', function () {
